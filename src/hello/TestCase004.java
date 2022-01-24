b16a5d2026b223efc936ac05d9655dfcf2e1b3f5
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-01-24 14:40:33 -0500

    requirement added

--- src/hello/TestCase004.java
+++ src/hello/TestCase004.java
@@ -5,3 +5,28 @@ public class TestCase004 {
 
 // hello
 //how are you
+//# GitHub Flow for Team collaboration
+//
+//1. create a local branch called `tc004`
+//2. create a java class `TestCase004`
+//3. make a commit
+//4. add one more change and make another commit
+//5. **DO NOT MERGE LOCALLY, DO NOT MOVE YOUR HEAD**
+//6. Push your local `tc004` branch to remote
+//   1. This will create `origin/tc004`
+//   2. Create connection between `tc004` and `origin/tc004`
+//   3. Push all the commits from `tc004` to `origin/tc004`
+//7. Now From GitHub.com repository main page We Open a request to merge our `origin/tc004` branch to `origin/master`
+//8. Such Request is known as `Pull Request` (really wished they called it `Merge Request` because it is)
+//9. Steps to open `Pull Request`
+//   1.  Go to your homepage of repository
+//   2.  If you have pushed your branch a moment ago , you should see **`Comapare and Pull Request`**
+//   3.  Click on it , it will take you to pull request page
+//   4.  Give a proper name for example : `Requesting to merge TC004 code`
+//   5.  Give some detail on the work been done
+//       1.  cool code here
+//       2.  cool code there
+//       3.  cool code everywhere
+//   6.  Click Create and now you have a open request.
+//   7.  This is where team can review your code and comment on your code or approve the changes and eventually merge this code to `origin/master` if all is well
+//   8.  One merge is complete , go back to your IntelliJ, checkout master and pull down all the changes happened in `origin/master`
