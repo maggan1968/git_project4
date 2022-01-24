e24d4a250027d2070edbb9bc49200473c4903ebe
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-01-24 10:34:36 -0500

    initial work is done

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,5 +5,25 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        //## Git Project 4
+        //
+        //1. create a java project called `git_practice4`
+        //2. create a `.gitignore` file with below content
+
+        //.idea
+        // out
+        // *.iml
+
+        //3. create a package under `src` folder called `hello` and create a class called `Hello`
+        //4. Enable version control to **create git repository** for this project
+        //5. Make a initial commit
+        //6. create a main method and add some print
+        //7. Make another commit with proper commit message
+        //8. Share project on Github (ONE TIME!!!!)
+        //9. Create a new branch called `tc001`
+        //10. Create a new class called `TestCase001` and make a commit with proper commit message
+        //11. Add 3 more changes and make 3 more individual commits
+        //12. And now merge your commits into master branch
     }
 }
