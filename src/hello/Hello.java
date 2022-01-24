2000f9a9f5338f07eef025461c2c7cccf35dcd7a
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-01-24 10:39:35 -0500

    doing final work on hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("b25 learning git and gitHub");
 
         //## Git Project 4
         //
@@ -21,9 +22,12 @@ public class Hello {
         //6. create a main method and add some print
         //7. Make another commit with proper commit message
         //8. Share project on Github (ONE TIME!!!!)
-        //9. Create a new branch called `tc001`
-        //10. Create a new class called `TestCase001` and make a commit with proper commit message
-        //11. Add 3 more changes and make 3 more individual commits
-        //12. And now merge your commits into master branch
+        //9. make some changes commit and push
+        //10. Create a new branch called `tc001`
+        //11. Create a new class called `TestCase001` and make a commit with proper commit message
+        //12. Add 3 more changes and make 3 more individual commits
+        //13. And now merge your commits into master branch
+
+
     }
 }
